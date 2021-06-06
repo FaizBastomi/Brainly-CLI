e2be11a52e6bac5861a66abf85dc76f3f0955e5d
ファイズ <64179402+FaizBastomi@users.noreply.github.com>
2021-06-07 00:25:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ apt-get install nodejs git -y
 git clone https://github.com/FaizBastomi/Brainly-CLI.git 'brainly'
 cd brainly
 npm install
-echo "Semua selesai. Sekarang kamu bisa memulai dengan 'npm start'" && cd brainly
+echo "Semua selesai. Sekarang kamu bisa 'cd brainly' dan memulai dengan 'npm start'"
