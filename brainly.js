6fab8a6046ac0f7a655164d7998e301387d011c7
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2021-07-08 18:00:15 +0700

    .
    Co-authored-by: FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>

--- brainly.js
+++ brainly.js
@@ -1,29 +1,29 @@
-const brainly = require('brainly-scraper-v2')
+const brainly = require('brainly-scraper-v2');
 
 /**
- * 
+ *
  * @param {String} query your question
  * @param {Number} total number of answer
  * @param {String} lang question and answer languages. Availabe language id, us, es, pt, ru, ro, tr, ph, pl, hi
- * @returns 
+ * @returns
  */
 const search = (query, total, lang) => {
     return new Promise(async (resolve, reject) => {
-        let okDB = []
-        const reS = await brainly(query, total, lang).catch(reject)
+        let okDB = [];
+        const reS = await brainly(query, total, lang).catch(reject);
 
-        let opt = {}
+        let opt = {};
         reS.data.forEach((r) => {
-            opt.pertanyaan = { pertanyaan: r.pertanyaan, media: r.questionMedia }
+            opt.pertanyaan = { pertanyaan: r.pertanyaan, media: r.questionMedia };
             r.jawaban.forEach((r) => {
-                opt.jawaban = { jawaban: r.text, media: r.media }
-            })
-            okDB.push(opt)
-        })
-        resolve(okDB)
-    })
-}
+                opt.jawaban = { jawaban: r.text, media: r.media };
+            });
+            okDB.push(opt);
+        });
+        resolve(okDB);
+    });
+};
 
 module.exports = {
     search
-}
+};
